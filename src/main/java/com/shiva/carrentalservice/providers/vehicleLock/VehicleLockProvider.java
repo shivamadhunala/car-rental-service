0c7c4408f3bb11ab3a28f56d5844a298af4a7509
shiva <shiva@smartinterviews.in>
2024-05-03 23:29:08 +0530

    Attempted to make some functions thread safe

--- src/main/java/com/shiva/carrentalservice/providers/vehicleLock/VehicleLockProvider.java
+++ src/main/java/com/shiva/carrentalservice/providers/vehicleLock/VehicleLockProvider.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.*;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class VehicleLockProvider implements IVehicleLockProvider {
 
@@ -23,19 +24,40 @@ public class VehicleLockProvider implements IVehicleLockProvider {
     private final int timeoutInSecs;
     private static volatile IVehicleLockProvider lockVehicleProvider;
 
+    private final HashMap<String, ReentrantReadWriteLock> reentrantReadWriteLockHashMap;
+
     private VehicleLockProvider(int timeoutInSecs) {
         this.vehicleWiseLockHashMap = new HashMap<>();
         this.vehicleLockHashMap = new HashMap<>();
         this.timeoutInSecs = timeoutInSecs;
+        reentrantReadWriteLockHashMap = new HashMap<>();
     }
 
     @Override
     public synchronized void lockVehicle(Vehicle vehicle, LocalDateTime from, LocalDateTime to, User user) {
-        long start = from.toInstant(ZoneOffset.UTC).toEpochMilli();;
-        VehicleLock vehicleLock = new VehicleLock(vehicle, timeoutInSecs, LocalDateTime.now(), from, to);
-        TreeMap<Long,VehicleLock> allLocks = vehicleWiseLockHashMap.computeIfAbsent(vehicle.getRegistrationId(), (k -> new TreeMap<>()));
-        allLocks.put(start, vehicleLock);
-        vehicleLockHashMap.put(vehicleLock.getId(), vehicleLock);
+        if(!this.reentrantReadWriteLockHashMap.containsKey(vehicle.getRegistrationId())){
+            this.createLock(vehicle);
+        }
+
+        ReentrantReadWriteLock.WriteLock writeLock = this.reentrantReadWriteLockHashMap.get(vehicle.getRegistrationId()).writeLock();
+        writeLock.lock();
+        try {
+            long start = from.toInstant(ZoneOffset.UTC).toEpochMilli();;
+            VehicleLock vehicleLock = new VehicleLock(vehicle, timeoutInSecs, LocalDateTime.now(), from, to);
+            TreeMap<Long,VehicleLock> allLocks = vehicleWiseLockHashMap.computeIfAbsent(vehicle.getRegistrationId(), (k -> new TreeMap<>()));
+            allLocks.put(start, vehicleLock);
+            vehicleLockHashMap.put(vehicleLock.getId(), vehicleLock);
+        } catch (Exception e) {
+
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    private synchronized void createLock(Vehicle vehicle) {
+        if(!this.reentrantReadWriteLockHashMap.containsKey(vehicle.getRegistrationId())) { // double locking check
+            this.reentrantReadWriteLockHashMap.put(vehicle.getRegistrationId(), new ReentrantReadWriteLock());
+        }
     }
 
     @Override
@@ -55,47 +77,61 @@ public class VehicleLockProvider implements IVehicleLockProvider {
             return false;
         }
 
-        TreeMap<Long,VehicleLock> allLocks = vehicleWiseLockHashMap.get(vehicle.getRegistrationId());
-        if(allLocks == null || allLocks.isEmpty()){
-            return false;
+        if(!this.reentrantReadWriteLockHashMap.containsKey(vehicle.getRegistrationId())){
+               this.createLock(vehicle);
         }
 
-        Map.Entry<Long, VehicleLock> floor = allLocks.floorEntry(from.toInstant(ZoneOffset.UTC).toEpochMilli());
-        if(floor == null) {
-            floor = allLocks.firstEntry();
-        }
+        ReentrantReadWriteLock.ReadLock readLock = this.reentrantReadWriteLockHashMap.get(vehicle.getRegistrationId()).readLock();
+        readLock.lock();
 
-        Map.Entry<Long, VehicleLock> entry = floor;
-
-        long fromTimeInMillis = from.toInstant(ZoneOffset.UTC).toEpochMilli();
-        long ToTimeInMillis = to.toInstant(ZoneOffset.UTC).toEpochMilli();
-
-        while(entry != null ) {
-
-            if(entry.getValue().isExpired()) {
-                continue;
+        try{
+            TreeMap<Long,VehicleLock> allLocks = vehicleWiseLockHashMap.get(vehicle.getRegistrationId());
+            if(allLocks == null || allLocks.isEmpty()){
+                return false;
             }
 
-            long existingFrom = entry.getKey();
-            long existingTo = entry.getValue().getTo().toInstant(ZoneOffset.UTC).toEpochMilli();;
+            Map.Entry<Long, VehicleLock> floor = allLocks.floorEntry(from.toInstant(ZoneOffset.UTC).toEpochMilli());
+            if(floor == null) {
+                floor = allLocks.firstEntry();
+            }
 
-            if(fromTimeInMillis <= existingFrom) {
-                if(existingFrom <= ToTimeInMillis){
-                    return true;
+            Map.Entry<Long, VehicleLock> entry = floor;
+
+            long fromTimeInMillis = from.toInstant(ZoneOffset.UTC).toEpochMilli();
+            long ToTimeInMillis = to.toInstant(ZoneOffset.UTC).toEpochMilli();
+
+            while(entry != null ) {
+
+                if(entry.getValue().isExpired()) {
+                    continue;
                 }
-            } else {
-                if(fromTimeInMillis <=  existingTo) {
-                    return true;
+
+                long existingFrom = entry.getKey();
+                long existingTo = entry.getValue().getTo().toInstant(ZoneOffset.UTC).toEpochMilli();;
+
+                if(fromTimeInMillis <= existingFrom) {
+                    if(existingFrom <= ToTimeInMillis){
+                        return true;
+                    }
+                } else {
+                    if(fromTimeInMillis <=  existingTo) {
+                        return true;
+                    }
                 }
+
+                if(ToTimeInMillis < existingFrom){
+                    break;
+                }
+
+                entry = allLocks.higherEntry(entry.getKey());
             }
 
-            if(ToTimeInMillis < existingFrom){
-                break;
-            }
 
-            entry = allLocks.higherEntry(entry.getKey());
+        } catch (Exception e) {
+
+        } finally {
+            readLock.unlock();
         }
-
         return false;
     }
 
